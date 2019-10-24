462dacbb83f08be8bc02a7fdecbc55cf6ab29d33
Jonathan Sack <jonathan.sack91@gmail.com>
2019-10-24 17:40:04 -0400

    Added models

--- City2City/Model/City.swift
+++ City2City/Model/City.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import CoreLocation
+
+class City {
+    
+    let name: String
+    let state: String
+    let population: String
+    let coordinates: CLLocationCoordinate2D
+    
+    init?(dict: [String:Any]) {
+        guard let name = dict["city"] as? String,
+            let state = dict["state"] as? String,
+            let long = dict["longitude"] as? Double,
+            let lat = dict["latitude"] as? Double,
+            let pop = dict["population"] as? String else { return nil }
+        
+        self.name = name
+        self.state = state
+        self.population = pop
+        self.coordinates = CLLocationCoordinate2D(latitude: lat, longitude: long)
+    }
+    
+    
+}
--- City2City/Model/Weather.swift
+++ City2City/Model/Weather.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class Weather {
+    
+    let main: String
+    let description: String
+    let temp: Double
+    let humidity: Int
+    let windSpeed: Double?
+    
+    
+    init?(dict: [String:Any]) {
+        
+        guard let weatherArray = dict["weather"] as? [[String:Any]],
+            let mainDict = dict["main"] as? [String:Any],
+            let windDict = dict["wind"] as? [String:Double],
+            let weatherDict = weatherArray.first else { return nil }
+        
+        self.main = weatherDict["main"] as! String
+        self.description = weatherDict["description"] as! String
+        self.temp = mainDict["temp"] as! Double
+        self.humidity = mainDict["humidity"] as! Int
+        self.windSpeed = windDict["speed"]
+    }
+    
+}
