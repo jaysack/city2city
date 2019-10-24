49f99da4b5bed9464e3d9337daba6218bce5273f
Jonathan Sack <jonathan.sack91@gmail.com>
2019-10-24 17:17:29 -0400

    Initial Commit

---
+++ City2City/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  City2City
+//
+//  Created by Jonathan Sack on 10/24/19.
+//  Copyright © 2019 Jonathan Sack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
