2d0efb3fcb7f4463605cca48f33b76ea9a9e1e68
Jonathan Sack <jonathan.sack91@gmail.com>
2019-10-24 17:23:35 -0400

    Setup project

---
+++ City2City/Model/City.swift
@@ -0,0 +1,9 @@
+//
+//  City.swift
+//  City2City
+//
+//  Created by Jonathan Sack on 10/24/19.
+//  Copyright © 2019 Jonathan Sack. All rights reserved.
+//
+
+import Foundation
---
+++ City2City/Model/Weather.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  City2City
+//
+//  Created by Jonathan Sack on 10/24/19.
+//  Copyright © 2019 Jonathan Sack. All rights reserved.
+//
+
+import Foundation
