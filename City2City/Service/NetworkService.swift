75a8bff63b78eb721d2f955533b718b74d96d03a
Jonathan Sack <jonathan.sack91@gmail.com>
2019-10-24 17:53:13 -0400

    Setup API request

---
+++ City2City/Service/NetworkService.swift
@@ -0,0 +1,54 @@
+//
+//  NetworkService.swift
+//  City2City
+//
+//  Created by Jonathan Sack on 10/24/19.
+//  Copyright © 2019 Jonathan Sack. All rights reserved.
+//
+
+import Foundation
+
+final class NetworkService {
+    
+    // MARK: - Singleton Stack
+    static let shared = NetworkService()
+    private init() {}
+    
+    
+    // MARK: - Fetch Weather
+    func getWeather(for city: City, completion: @escaping (Weather?) -> Void) {
+        
+        // Set URL
+        guard let url = URL(string: "\(ENDPOINT.WEATHER)&lat=\(city.coordinates.latitude)&lon=\(city.coordinates.longitude)") else {
+            print("JSError: Endpoint not valid")
+            return
+        }
+        
+        // URLSession
+        URLSession.shared.dataTask(with: url) { (dat, _, err) in
+            
+            // Error handling
+            if let error = err {
+                print("JSError: Unable to fetch weather ~> \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            // Decode JSON
+            if let data = dat {
+                do {
+                    let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+                    let weather = Weather(dict: jsonResponse)
+                    completion(weather)
+                    
+                } catch {
+                    print("JSError: Unable to serialize json object ~> \(error.localizedDescription)")
+                    completion(nil)
+                    return
+                }
+            }
+            
+        }.resume()
+    }
+    
+}
---
+++ City2City/Utilities/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  City2City
+//
+//  Created by Jonathan Sack on 10/24/19.
+//  Copyright © 2019 Jonathan Sack. All rights reserved.
+//
+
+import Foundation
+
+struct ENDPOINT {
+    
+    static let BASE = "https://api.openweathermap.org/data/2.5/weather"
+    static let WEATHER = "\(ENDPOINT.BASE)?units=imperial"
+}
